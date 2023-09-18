888b9c6188c86375f1e50644d3595383590ca811
fresnelll <75586758+fresnelll@users.noreply.github.com>
2023-09-18 12:05:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,51 @@
+# Alex version of main.py (works)
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port, Stop
+from pybricks.tools import wait
+import functions as f
+
+# Initialize the EV3 brick.
+ev3 = EV3Brick()
+ev3.screen.print("STARTED")
+wait(3000)
+ev3.screen.clear()
+
+# Define and initialize the motors and color sensors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+left_color_sensor = ColorSensor(Port.S4)
+right_color_sensor = ColorSensor(Port.S1)
+
+# Define color thresholds for black+6, white, and blue lines.
+BLACK_THRESHOLD_L = 30
+BLACK_THRESHOLD_R = 30
+WHITE_THRESHOLD_L = 300
+WHITE_THRESHOLD_R = 300
+BLUE_THRESHOLD = 150  # Adjust this threshold based on sensor readings.
+
+# PID constants (adjust as needed).
+KP = 1.0  # Proportional gain
+KI = 0.05  # Integral gain
+KD = 0.0  # Derivative gain
+
+# Anti-windup parameters.
+MAX_INTEGRAL = 1000  # Maximum allowable integral term value.
+MIN_INTEGRAL = -1000  # Minimum allowable integral term value.
+
+# Speed for slow movement and speed increment.
+SLOW_SPEED = 50  # Adjust this value for your robot.
+SPEED_INCREMENT = 5  # Adjust this value for smaller steps.
+
+# Call the function to start line following with PID control.
+f.line_following_pid(ev3, left_motor, right_motor, left_color_sensor, right_color_sensor,
+                     BLACK_THRESHOLD_L, BLACK_THRESHOLD_R, WHITE_THRESHOLD_L, WHITE_THRESHOLD_R,
+                     BLUE_THRESHOLD, KP, KI, KD, MAX_INTEGRAL, MIN_INTEGRAL, SLOW_SPEED, SPEED_INCREMENT)
+                     
+
+"""
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
@@ -24,7 +72,7 @@ LM = Motor(Port.B)
 CR = ColorSensor(Port.S1)
 CL = ColorSensor(Port.S4)
 G = GyroSensor(Port.S3)
-
+"""
 
 """ START """ 
 #ev3.speaker.beep()
